b676e2e03d21ab453bcf97a1bde337f561135c0b
Joshua Behrens <behrens@heptacom.de>
2020-05-14 12:26:26 +0200

    Add controller to interact with default language switcher service

---
+++ src/Core/System/Language/Api/LanguageDefaultSwitcherActionController.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\System\Language\Api;
+
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\System\Language\SwitchDefault\LanguageDefaultSwitchInvoker;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @RouteScope(scopes={"api"})
+ */
+class LanguageDefaultSwitcherActionController extends AbstractController
+{
+    /**
+     * @var LanguageDefaultSwitchInvoker
+     */
+    private $invoker;
+
+    public function __construct(LanguageDefaultSwitchInvoker $invoker)
+    {
+        $this->invoker = $invoker;
+    }
+
+    /**
+     * @Route("/api/v{version}/_action/language-default/switch/{languageId}", name="api.action.language-default.switch", methods={"POST"})
+     */
+    public function switchDefaultLanguage(string $languageId): JsonResponse
+    {
+        $this->invoker->invokeLanguageSwitch($languageId);
+
+        return new JsonResponse([
+            'success' => true,
+        ], Response::HTTP_CREATED);
+    }
+}
